fbf16668b19b0768df54525e068cf3363f984603
lyg <liyuangang66@gmail.com>
2021-03-05 12:19:55 +0800

    User类的创建

--- StudyBase/ExamSystemClient/src/model/User.java
+++ StudyBase/ExamSystemClient/src/model/User.java
@@ -8,7 +8,28 @@ public class User implements Serializable {
     private String userName;
     private String passWord;
 
+    public User(String userName, String passWord) {
+        this.userName = userName;
+        this.passWord = passWord;
+    }
+
+    public User() {
+    }
 
 
+    public String getUserName() {
+        return userName;
+    }
 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
 }
