6aabb3feac5b74ff679604cc96e64f6986aa0cf4
lyg <liyuangang66@gmail.com>
2021-03-09 19:42:18 +0800

    Mysql

--- Mysql/src/main/java/JDBCDemo01.java
+++ Mysql/src/main/java/JDBCDemo01.java
@@ -1,16 +1,27 @@
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JDBCDemo01 {
     public static void main(String[] args) throws Exception {
         //1.注册驱动(可省略)
-        Class.forName("com.mysql.jdbc.driver");
+        Class.forName("com.mysql.jdbc.Driver");
         //2.获取连接 connection连接对象
         String url = "jdbc:mysql://localhost:3306/paotui?characterEncoding=UTF-8";
         Connection connection = DriverManager.getConnection(url, "root", "root");
 
-        System.out.println(connection);
+        //3.获取语句执行平台 statement
+        Statement statement = connection.createStatement();
+
+        //3.通过statement对象的executeUpdate方法创建一张表
+        String sql = "create table test(id int,name varchar(20),age int)";
+
+        //返回值表示受影响的行数
+        int i = statement.executeUpdate(sql);
+        System.out.println(i);
+
+        //3.2
+
+        //关闭流
+        statement.close();
+        connection.close();
     }
 }
