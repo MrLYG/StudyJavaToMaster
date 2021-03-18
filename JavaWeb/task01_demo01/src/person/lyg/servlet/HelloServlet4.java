929a285440cb50d51161d22a975b2d8f59e900e2
lyg <liyuangang66@gmail.com>
2021-03-18 23:34:17 +0800

    Servlet1

---
+++ JavaWeb/task01_demo01/src/person/lyg/servlet/HelloServlet4.java
@@ -0,0 +1,26 @@
+package person.lyg.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloServlet4")
+public class HelloServlet4 extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+}
