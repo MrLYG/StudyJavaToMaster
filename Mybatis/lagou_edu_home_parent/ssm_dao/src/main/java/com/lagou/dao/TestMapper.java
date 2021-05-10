89bd4c27ab530dbfda86f818db333700a370f552
lyg <liyuangang66@gmail.com>
2021-05-10 19:12:27 +0800

    SSM教育系统初步搭建完成

---
+++ Mybatis/lagou_edu_home_parent/ssm_dao/src/main/java/com/lagou/dao/TestMapper.java
@@ -0,0 +1,4 @@
+package com.lagou.dao;
+
+public interface TestMapper {
+}
---
+++ Mybatis/lagou_edu_home_parent/ssm_service/src/main/java/com/lagou/service/TestService.java
@@ -0,0 +1,9 @@
+package com.lagou.service;
+
+/**
+ * @Author: Ken
+ * @Date: 2021/5/10 15:40
+ */
+public interface TestService {
+
+}
